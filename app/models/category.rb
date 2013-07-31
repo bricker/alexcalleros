2ec847a318c505c0e4e35a378b9bf306364aef96
Bryan Ricker <bricker88@gmail.com>
2013-07-31 01:40:35 -0700

    Add some validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :videos
+
+  validates_presence_of :title, :key
+  validates_uniqueness_of :key
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,4 @@
 class Content < ActiveRecord::Base
+  validates_presence_of :key
+  validates_uniqueness_of :key
 end
