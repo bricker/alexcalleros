ea0714a512d9a2750d8c10ac8a9bb02e97a2e81e
Bryan Ricker <bricker88@gmail.com>
2013-07-31 01:40:27 -0700

    Add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  BOOTSTRAP_ALERT_MAP = {
+    :notice => "success",
+    :alert  => "danger"
+  }
+
+  def bootstrap_alert_class(key)
+    BOOTSTRAP_ALERT_MAP[key.to_sym] || key.to_s
+  end
+
+  def active_for(name)
+    "active" if controller_name == name
+  end
+
+  def form_errors(form)
+    render '/admin/shared/form_errors', f: form
+  end
+
+  def delete_tag(object)
+    render '/admin/shared/delete_tag', object: object
+  end
 end
