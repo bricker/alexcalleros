c3584ce4b400bef9790c51631f443aae423ec49f
Bryan Ricker <bricker88@gmail.com>
2013-08-01 01:27:28 -0700

    Parameterize slug before validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,16 @@ class Category < ActiveRecord::Base
 
   validates_presence_of :title, :slug
   validates_uniqueness_of :slug
+
+  before_validation :parameterize_slug
+
+  private
+
+  def parameterize_slug
+    if self.slug.present?
+      self.slug = self.slug.parameterize
+    else
+      self.slug = self.title.parameterize
+    end
+  end
 end
