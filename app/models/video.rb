3e7bf0e81131c50ca5ecefefedb4a7ed87ffb7cf
Bryan Ricker <bricker88@gmail.com>
2013-08-01 01:29:29 -0700

    Add source/ID matchers for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,28 @@
 class Video < ActiveRecord::Base
+  URL_MATCHERS = {
+    "youtube" => %r{youtube\.com/watch?v=(?<id>\w)},
+    "vimeo"   => %r{}
+  }
+
+  SOURCE_MATCHERS = {
+    %r{youtube} => "youtube",
+    %r{vimeo}   => "vimeo"
+  }
+
   belongs_to :category
   validates_presence_of :title, :url
   mount_uploader :thumbnail, ThumbnailUploader
 
   before_validation :parameterize_slug
 
+
+  def external_id
+    @external_id ||= self.url.match(URL_MATCHERS[self.source])[:id]
+  end
+
+  def source
+    @source ||= SOURCE_MATCHERS.find { |k, _| self.url.match(k) }[1]
+  end
 
 
   private
